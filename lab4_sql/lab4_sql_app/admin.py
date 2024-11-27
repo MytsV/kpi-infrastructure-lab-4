26897878c268bd0d2b08728b9549f7ee7ff74c12
Oleksandr <sa44ko@gmail.com>
2024-11-27 13:47:53 +0200

    Improved display settings in admin panel for product entity.

--- lab4_sql/lab4_sql_app/admin.py
+++ lab4_sql/lab4_sql_app/admin.py
@@ -7,11 +7,16 @@ class ClientAdmin(admin.ModelAdmin):
     search_fields = ('id', 'name')
     list_filter = ('gender',)
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'price')
+    search_fields = ('id', 'type', 'price')
+    list_filter = ('type',)
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('client_id', 'product_id')
     search_fields = ('client_id', 'product_id')
     list_filter = ('client_id', 'product_id')
 
 admin.site.register(Client, ClientAdmin)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order, OrderAdmin)
