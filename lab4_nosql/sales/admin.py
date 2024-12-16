d7dc7e22700e6ebe5423828c41a7007743d3a3a8
MytsV <mytsvictoria@gmail.com>
2024-12-16 23:03:27 +0200

    Implement image handling in the nosql application

--- lab4_nosql/sales/admin.py
+++ lab4_nosql/sales/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Salesperson
 
 class SalespersonAdmin(admin.ModelAdmin):
-    list_display = ('code', 'full_name', 'age', 'gender', 'phone_number', 'date_joined')
+    list_display = ('id', 'full_name', 'age', 'gender', 'phone_number', 'date_joined', 'picture_tag')
     search_fields = ('full_name', 'code')
     list_filter = ('gender',)
 
--- lab4_nosql/sales/models.py
+++ lab4_nosql/sales/models.py
@@ -1,11 +1,16 @@
 from django.core.validators import RegexValidator
 from django.db import models
-import uuid
+
+from django.utils.safestring import mark_safe
+from django.conf import settings
+
 
 class Salesperson(models.Model):
-    code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    id = models.AutoField(primary_key=True)
 
     full_name = models.CharField(max_length=100)
+
+    picture = models.ImageField(upload_to='images/', blank=True)
 
     age = models.PositiveIntegerField()
 
@@ -22,8 +27,14 @@ class Salesperson(models.Model):
 
     date_joined = models.DateField()
 
+    def picture_tag(self):
+        if self.picture:
+            return mark_safe('<img src="%s%s" width="150" height="150" />' % (settings.MEDIA_URL, self.picture))
+        else:
+            return mark_safe('<span>No picture available</span>')
+
     def __str__(self):
-        return f"{self.full_name} ({self.code})"
+        return f"{self.full_name} ({self.id})"
 
     class Meta:
         verbose_name = 'Salesperson'
